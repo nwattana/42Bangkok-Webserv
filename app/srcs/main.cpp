0a03f844df9f8cc8fbe9ceea1aafcf4a881976b7
OmegaWiz <kraiwish123@gmail.com>
2024-03-11 15:40:11 +0700

    feat: :sparkles: add dummy connection

--- app/srcs/main.cpp
+++ app/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kkaiyawo <kkaiyawo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 13:52:10 by kkaiyawo          #+#    #+#             */
-/*   Updated: 2024/03/11 11:51:03 by kkaiyawo         ###   ########.fr       */
+/*   Updated: 2024/03/11 15:39:40 by kkaiyawo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,42 @@ int main(int argc, char **argv)
 
 	ConfigParser config(filename);
 	config.printServerConfig();
+	std::cout << std::endl;
 	//create server
 	int server_fd;
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		exitWithError();
+		exitWithError("Error: unable to create a socket", 1);
 	}
 
-	//run server
+	sockaddr_in sockaddr;
+	sockaddr.sin_family = AF_INET;
+	sockaddr.sin_port = htons(8080);
+	sockaddr.sin_addr.s_addr = INADDR_ANY;
+	if ((bind(server_fd, (struct sockaddr *)&sockaddr, sizeof(sockaddr))) < 0) {
+		exitWithError("Error: unable to bind to port 8080", 1);
+	}
 
+	if ((listen(server_fd, 3)) < 0) {
+		exitWithError("Error: unable to listen", 1);
+	}
+
+	int acceptedSocket;
+	if ((acceptedSocket = accept(server_fd, (struct sockaddr *)&sockaddr, (socklen_t *)&sockaddr)) < 0) {
+		exitWithError("Error: unable to accept connection", 1);
+	}
+
+	char buffer[100];
+	if (read(acceptedSocket, buffer, 100) < 0) {
+		exitWithError("Error: unable to read from socket", 1);
+	}
+	std::cout << "Message from client: " << buffer << std::endl;
+
+	std::string response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+	if (send(acceptedSocket, response.c_str(), response.length(), 0) < 0) {
+		exitWithError("Error: unable to send response", 1);
+	}
+
+	close(acceptedSocket);
 	close(server_fd);
 	std::cout << "\nEnd program all Success\n" << std::endl;
 	return (0);
